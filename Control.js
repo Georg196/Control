31f42432e05635a30ae15d12a13c2657863061d0
George Kondrashevsky <146424140+Georg196@users.noreply.github.com>
2025-09-24 10:43:09 +0500

    Update Control.js

--- Control.js
+++ Control.js
@@ -1,1 +1,11 @@
-console.log('Ozonbox74 - Челябинск')
+x = window.location.href
+
+if (~x.indexOf("ozonbox.pro")){
+  console.log('Ozonbox74 - Челябинск')  
+} else if (~x.indexOf("olivedaddy.ru")){
+  console.log('Оливковый Папа') 
+} else if (~x.indexOf(" ")){
+  console.log(x)
+} 
+
+
