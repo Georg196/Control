49f5b90969f77a5e0a5f4c276c09e2bca8b620e6
George Kondrashevsky <146424140+Georg196@users.noreply.github.com>
2026-02-18 17:05:52 +0500

    Update Control.js

--- Control.js
+++ Control.js
@@ -1,11 +1,11 @@
 x = window.location.href
 
-if (~x.indexOf("ozonbox.pro")){
-  console.log('Ozonbox74 - Челябинск')  
-} else if (~x.indexOf("olivedaddy.ru")){
-  console.log('Оливковый Папа') 
-} else if (~x.indexOf(" ")){
-  console.log(x)
-} 
+// if (~x.indexOf("ozonbox.pro")){
+//   // console.log('Ozonbox74 - Челябинск')  
+// } else if (~x.indexOf("olivedaddy.ru")){
+//   // console.log('Оливковый Папа') 
+// } else if (~x.indexOf(" ")){
+//   // console.log(x)
+// } 
 
 
